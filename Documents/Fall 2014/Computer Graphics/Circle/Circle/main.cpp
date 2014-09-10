5883a43ff3923d27a6300e73aea8504c5013e3c2
Alecia McEachran <alecia.mceachran@drake.edu>
2014-09-09 20:15:27 -0500

    added a comment

---
+++ Documents/Fall 2014/Computer Graphics/Circle/Circle/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Circle
+//
+//  Created by Alecia on 9/8/14.
+//  Copyright (c) 2014 Alecia McEachran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
